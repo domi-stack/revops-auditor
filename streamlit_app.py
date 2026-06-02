6825bdcca05069a9360553c6e20b9efd24ef44dc
Domingo Serra <ssdomi9@gmail.com>
2026-06-02 13:25:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,14 @@ if file:
 
     st.subheader("Unit Economics")
     st.json(unit_economics(10000, 50, 1200, 12, 100))
+    st.subheader("AI Insights")
+
+funnel_data = funnel_conversion(df)
+unit_data = unit_economics(10000, 50, 1200, 12, 100)
+
+insights = generate_insights(funnel_data, unit_data)
+
+for i in insights:
+    st.write(i)
 else:
     st.info("Upload CSV to start")
